7c193650be85f05f663d369dca05f57969f55959
abel enrique <abel@abel.com>
2020-02-20 17:49:04 -0600

    estilo del contenido principal

--- index.php
+++ index.php
@@ -66,27 +66,28 @@
 
                 <!-- contenido central -->
                 <div id="central">
-
+                    <h1>Productos destacados</h1>
+                    
                     <div class="product">
                         <img src="assets/img/camiseta.png" />
                         <h2>Camiseta Azul Ancha</h2>
-                        <p>30 euros</p>
-                        <a href="">Comprar</a>
+                        <p>30 pesos</p>
+                        <a href="" class="button">Comprar</a>
                     </div>
 
 
                     <div class="product">
                         <img src="assets/img/camiseta.png" />
                         <h2>Camiseta Azul Ancha</h2>
-                        <p>30 euros</p>
-                        <a href="">Comprar</a>
+                        <p>30 pesos</p>
+                        <a href="" class="button">Comprar</a>
                     </div>
 
                     <div class="product">
                         <img src="assets/img/camiseta.png" />
                         <h2>Camiseta Azul Ancha</h2>
-                        <p>30 euros</p>
-                        <a href="">Comprar</a>
+                        <p>30 pesos</p>
+                        <a href="" class="button">Comprar</a>
                     </div>
 
 
