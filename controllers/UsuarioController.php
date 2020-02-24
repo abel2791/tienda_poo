614811b5a964041436a8b86aba99a0e4f183df65
abel enrique <abel@abel.com>
2020-02-23 20:37:18 -0600

    se agrego la vista usuario y se hizo el formulario de registro de usuarios

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -1,7 +1,16 @@
 <?php
 
 class UsuarioController{
+    
     public function index(){
         echo 'controlador Usuarios, accion index';
     }
+    
+    public function registro(){
+        require_once 'views/usuario/registro.php'; 
+     }
+     
+     public function save(){
+         
+     }
 }
