1df925b18da7fd89d8323037e3189f24fdbd7353
abel enrique <abel@abel.com>
2020-02-23 22:43:38 -0600

    se agrego rutas amigables .htaccess

--- index.php
+++ index.php
@@ -3,15 +3,25 @@
 // de lo que me llega por la url, es el unico fichero que se encarga de cargarlo absolutamente todo
 //para arrancar el mvc pones en la url: http://localhost/master-php/aprendiendo-php-mvc/?controller=Usuario&action=mostrarTodos
 require_once 'autoload.php';
+require_once 'config/parameters.php';
 require_once 'views/layout/header.php';
 require_once 'views/layout/sidebar.php';
 
+function show_error(){
+    $error = new ErrorController();
+    $error->index();
+}
 
 
 if(isset($_GET['controller'])){
       $nombre_controlador = $_GET['controller'].'Controller';
- }else{
-      echo 'La pagina no existe';
+ }elseif(!isset ($GET['controller']) && !isset ($GET['action'])){
+     $nombre_controlador = controller_default;
+ }
+ 
+ 
+ else{
+     show_error();
       exit();//el exit para que pare la ejecucion y lo de abajo ya no lo ejecute
 }
     
@@ -26,12 +36,16 @@ if(isset($_GET['action']) && method_exists($controlador, $_GET['action'])){//si 
     
     //ponemos esto en la url para llamar a los metodos "?action=crear"
     
-    }else{
-        echo 'La pagina no existe';
+    }elseif(!isset ($GET['controller']) && !isset ($GET['action'])){
+        $action_default = action_default;
+        $controlador->$action_default();        
+    }
+     else{
+       show_error();
     }
  
    }else{
-    echo 'La pagina no existe';
+    show_error();
 }
 
 require_once 'views/layout/footer.php';
--- views/layout/header.php
+++ views/layout/header.php
@@ -3,14 +3,14 @@
     <head>
         <meta charset="utf-8" />
         <title>Moda KikSport</title>
-        <link rel="stylesheet" href="assets/css/styles.css" />
+        <link rel="stylesheet" href="<?=base_url?>assets/css/styles.css" />
     </head>
     <body>
         <div id="container">
             <!-- cabecera -->
             <header id="header">
                 <div id="logo">
-                    <img src="assets/img/camiseta.png" alt="Camiseta Logo" />
+                    <img src="<?=base_url?>assets/img/camiseta.png" alt="Camiseta Logo" />
                     <a href="index.php">
                         Moda KikSport
                     </a>
