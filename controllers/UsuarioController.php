e11a17ef83a0fbea2e03af9d9fffe60bc404623d
abel enrique <abel@abel.com>
2020-02-28 22:06:54 -0600

    mejoras en el menu

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -55,7 +55,7 @@ class UsuarioController{
                if($identity && is_object($identity)){
                    $_SESSION['identity'] = $identity;
                    
-                   if($identity->role == 'admin'){
+                   if($identity->rol == 'admin'){
                        $_SESSION['admin'] = true;
                    }
                }else{
--- views/layout/sidebar.php
+++ views/layout/sidebar.php
@@ -13,14 +13,21 @@
                            <input type="submit" value="Enviar" />                         
                        </form>
                        <?php else: ?>
-                         <h3><?=$_SESSION['identity']->nombre?>-<?=$_SESSION['identity']->apellidos?></h3>
+                         <h3><?=$_SESSION['identity']->nombre?> <?=$_SESSION['identity']->apellidos?></h3>
                        <?php endif; ?>
 
                        <ul>
-                           <li><a href="#">Mis pedidos</a></li>
-                           <li><a href="#">Gestionar pedidos</a></li>
-                           <li><a href="#">Gestionar categorias</a></li>
-                           <li><a href="<?=base_url?>Usuario/logout">Cerrar sesion</a></li>
+                           
+                           <?php if(isset($_SESSION['admin'])):?> 
+                                <li><a href="#">Gestionar categorias</a></li>
+                                <li><a href="#">Gestionar productos</a></li>
+                                <li><a href="#">Gestionar pedidos</a></li>                                
+                           <?php endif;?>
+                                
+                                <?php if(isset($_SESSION['identity'])): ?>
+                                <li><a href="#">Mis pedidos</a></li>
+                                <li><a href="<?=base_url?>Usuario/logout">Cerrar sesion</a></li>
+                                <?php endif;?>
                        </ul>
                    </div>
 
