bb2abe2d42a94ba40de9b76d2eb72e2157073133
abel <ing.enrique_gallardo.com>
2020-03-22 20:46:20 -0600

    detalle del producto

--- views/categoria/ver.php
+++ views/categoria/ver.php
@@ -3,20 +3,22 @@
     <?php if ($productos->num_rows == 0) : ?>
         <p>No hay productos para mostrar</p>
     <?php else: ?>
-        
+
         <?php while ($product = $productos->fetch_object()): ?>
             <div class="product">
-                <?php if ($product->imagen != null): ?>
-                    <img src="<?= base_url ?>uploads/images/<?= $product->imagen ?>" />
-                <?php else: ?>
-                    <img src="<?=base_url?>assets/img/camiseta.png" />
-                <?php endif; ?>
-                <h2><?= $product->nombre ?></h2>
+                <a href="<?= base_url ?>Producto/ver&id=<?= $product->id ?>">
+                    <?php if ($product->imagen != null): ?>
+                        <img src="<?= base_url ?>uploads/images/<?= $product->imagen ?>" />
+                    <?php else: ?>
+                        <img src="<?= base_url ?>assets/img/camiseta.png" />
+                    <?php endif; ?>
+                    <h2><?= $product->nombre ?></h2>
+                </a>
                 <p><?= $product->precio ?></p>
                 <a href="" class="button">Comprar</a>
             </div>
         <?php endwhile; ?>
-        
+
     <?php endif; ?>
 <?php else: ?>
     <h1>La categoria no existe</h1>
